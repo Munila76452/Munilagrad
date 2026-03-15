db097421a4ba4ade05c56f525a313dec3741e24a
Munila76452 <your.email@example.com>
2026-03-15 13:42:34 +0530

    Initial commit: MiniGrad implementation with MLP

---
+++ munilagrad/test.py
@@ -0,0 +1,22 @@
+from engine import value
+from nn import MLP
+xs = [
+    [2.0,3.0,-1.0],
+    [3.0,-1.0,0.5],
+    [0.5,1.0,1.0],
+    [1.0,1.0,-1.0]
+]
+
+ys = [1.0,-1.0,-1.0,1.0]
+
+n = MLP(3,[4,4,1])
+for k in range(20):
+    ypred = [n([value(xi) for xi in x]) for x in xs]
+    loss = sum(((yout - value(ygt))**2 for ygt,yout in zip(ys,ypred)), value(0))
+    for p in n.parameters():
+        p.grad = 0.0
+    loss.backward()
+    for p in n.parameters():
+        p.data += -0.05 * p.grad
+
+    print(k, loss.data)

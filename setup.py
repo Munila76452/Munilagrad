edcd6deedd42ede1afc4650eda2692317e726118
Munila76452 <your.email@example.com>
2026-03-18 10:22:15 +0530

    Chore: Update setup.py for v0.1.0 PyPI release and ignore build artifacts

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup, find_packages
 
+# This reads your README.md to use as the long description on PyPI
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="munilagrad",
-    version="0.1.0",
+    name="munilagrad", # This is the name people will type in pip install
+    version="0.1.0",   # You must change this number every time you push an update
     author="Tanish Taywade",
     description="A custom autograd engine and deep learning framework built from scratch",
-    packages=find_packages(), # This automatically finds your 'munilagrad' folder
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Munila76452/Munilagrad", # Link to your GitHub
+    packages=find_packages(), 
     install_requires=[
-        "graphviz" # Automatically installs graphviz when someone installs your package
+        "graphviz" 
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
 )

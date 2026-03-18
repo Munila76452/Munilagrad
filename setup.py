88c74504272c933051f70543e0555c5fea45162f
tanish taywade <155261572+Munila76452@users.noreply.github.com>
2026-03-18 09:57:49 +0530

    Update setup.py
    Updated comments for clarity in setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version="0.1.0",
     author="Tanish Taywade",
     description="A custom autograd engine and deep learning framework built from scratch",
-    packages=find_packages(), # This automatically finds your 'munilagrad' folder
+    packages=find_packages(),
     install_requires=[
-        "graphviz" # Automatically installs graphviz when someone installs your package
+        "graphviz" # it automatically installs when anyone download munilagrad
     ],
     python_requires=">=3.6",
 )
